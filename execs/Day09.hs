ac29562a19fc40a6c5f8dcc714ccb19df6dccf1b
Eric Mertens <emertens@galois.com>
2021-12-09 11:12:17 -0800

    Update day 9

--- execs/Day09.hs
+++ execs/Day09.hs
@@ -12,39 +12,50 @@ Find the basins on the height map.
 
 -}
 module Main (main) where
-import Advent (cardinality)
+
+import Advent (cardinality, getInputArray, arrIx)
 import Advent.Coord (Coord(..), cardinal)
-import Advent.Format (format)
+import Data.Array.Unboxed (Array, UArray, (!), array, assocs, bounds, elems)
 import Data.Char (digitToInt)
-import Data.List (sort)
-import Data.Map (Map)
-import Data.Map qualified as Map
+import Data.Foldable (toList)
+import Data.List (sortBy)
+import Data.Maybe (catMaybes)
+
+type Heights  = Array Coord (Maybe Int)
+type BasinIds = Array Coord (Maybe Coord)
 
 -- >>> :main
 -- 588
 -- 964712
 main :: IO ()
 main =
-  do  inp <- [format|9 (%s%n)*|]
-      let m = Map.fromList
-                [ (C y x, digitToInt z)
-                | (y,xs) <- zip [0..] inp
-                , (x,z ) <- zip [0..] xs
-                , z /= '9']
+  do  heights <- heightArray <$> getInputArray 9
+      let basinIds = heightsToBasinIds heights
 
-      print (sum [1+h | (c,h) <- Map.toList m, null (downs m c h)])
+      print (sum [1+h | (c, Just h) <- assocs heights, Just c == basinIds!c])
+      print (product (top 3 (basinSizes basinIds)))
 
-      let basinId = Map.mapWithKey part2 m
-          part2 c h =
-            do xs <- traverse (basinId Map.!) (downs m c h)
-               case xs of
-                 [] -> Just c
-                 y:ys | all (y==) ys -> Just y
-                      | otherwise    -> Nothing
+-- | Returns the @n@ largest elements of a list
+top :: Ord a => Int -> [a] -> [a]
+top n = take n . sortBy (flip compare)
 
-      let top n = take n . reverse . sort
-      print $ product $ top 3
-            $ Map.elems $ cardinality $ Map.elems $ Map.mapMaybe id basinId
+-- | Convert that ASCII grid into a grid of heights
+heightArray :: UArray Coord Char -> Heights
+heightArray a = array (bounds a) [(c, cvt x) | (c, x) <- assocs a]
+  where
+    cvt '9' = Nothing -- 9s are defined to be out of bounds in the problem
+    cvt x   = Just (digitToInt x)
 
-downs :: Map Coord Int -> Coord -> Int -> [Coord]
-downs m c h = [x | x <- cardinal c, Map.findWithDefault 9 x m < h]
+heightsToBasinIds :: Heights -> BasinIds
+heightsToBasinIds heights = basinIds
+  where
+    basinIds = array (bounds heights) [(c, basinId c =<< mbh) | (c, mbh) <- assocs heights]
+    basinId c h =
+      do xs <- sequence [basinIds!x | x <- cardinal c, Just (Just xh) <- [arrIx heights x], xh<h]
+         case xs of
+           []                  -> Just c -- this is the lowest point in the basin
+           y:ys | all (y==) ys -> Just y -- all flows go to a unique basin
+                | otherwise    -> Nothing -- no unique basin
+
+basinSizes :: BasinIds -> [Int]
+basinSizes = toList . cardinality . catMaybes . elems
