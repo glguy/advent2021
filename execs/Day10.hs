687594079b472d1aabf4a8dfbba61c9c1b2ef394
Eric Mertens <emertens@gmail.com>
2021-12-11 15:54:36 -0800

    comments

--- execs/Day10.hs
+++ execs/Day10.hs
@@ -22,18 +22,30 @@ main =
   do inp <- getInputLines 10
      let (p1, p2) = partitionEithers (validate [] <$> inp)
      print (sum (map cost1 p1))
-     print (middle (map cost2 p2))
+     print (median (map cost2 p2))
 
 -- | Return the median of an odd-length list
-middle :: Ord a => [a] -> a
-middle xs = sort xs !! (length xs `div` 2)
+--
+-- >>> median [1,2,0]
+-- 1
+median :: Ord a => [a] -> a
+median xs = sort xs !! (length xs `div` 2)
 
+-- | Either find the first unexpected bracket, or return the expected
+-- sequence of closing characters.
+--
+-- >>> validate [] "{([(<{}[<>[]}>{[]{[(<()>"
+-- Left '}'
+--
+-- >>> validate [] "[({(<(())[]>[[{[]{<()<>>"
+-- Right "}}]])})]"
 validate :: String -> String -> Either Char String
 validate (x:xs) (y:ys) | x == y            = validate xs ys
 validate xs     (y:ys) | Just x <- close y = validate (x:xs) ys
 validate _      (y:_ )                     = Left y
 validate xs     []                         = Right xs
 
+-- | Return the character that closes this bracket, if there is one.
 close :: Char -> Maybe Char
 close '(' = Just ')'
 close '[' = Just ']'
@@ -48,6 +60,10 @@ cost1 '}' = 1197
 cost1 '>' = 25137
 cost1 x   = error ("cost1: bad input " ++ show x)
 
+-- | Compute the part 2 cost of the expected tail.
+--
+-- >>> cost2 "}}]])})]"
+-- 288957
 cost2 :: String -> Int
 cost2 = fromDigits 5 . map cost2'
 
