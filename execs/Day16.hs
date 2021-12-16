b2a836955c6ce21975aa2593558f57c183052a2c
Eric Mertens <emertens@galois.com>
2021-12-16 10:11:49 -0800

    comments

--- execs/Day16.hs
+++ execs/Day16.hs
@@ -1,4 +1,4 @@
-{-# Language ImportQualifiedPost, QuasiQuotes #-}
+{-# Language ImportQualifiedPost, LambdaCase, QuasiQuotes, ViewPatterns, BlockArguments #-}
 {-|
 Module      : Main
 Description : Day 16 solution
@@ -10,11 +10,55 @@ Maintainer  : emertens@gmail.com
 
 Decode an expression from a bitstream.
 
+This solution uses the ReadP parser combinator module.
+Since ReadP only operates on 'String' parsing bits is
+simulated by parsing strings of the characters @'0'@ and @'1'@.
+ReadP's 'ReadP.gather' operation is useful for computing
+the bitsize of a packet which comes up in some of the
+operator packets.
+
+= Examples 
+
+>>> vers <$> parse (decodeHex "8A004A801A8002F478")
+Just 16
+
+>>> vers <$> parse (decodeHex "620080001611562C8802118E34")
+Just 12
+
+>>> vers <$> parse (decodeHex "C0015000016115A2E0802F182340")
+Just 23
+
+>>> vers <$> parse (decodeHex "A0016C880162017C3686B18A3D4780")
+Just 31
+
+>>> eval <$> parse (decodeHex "C200B40A82")
+Just 3
+
+>>> eval <$> parse (decodeHex "04005AC33890")
+Just 54
+
+>>> eval <$> parse (decodeHex "880086C3E88112")
+Just 7
+
+>>> eval <$> parse (decodeHex "CE00C43D881120")
+Just 9
+
+>>> eval <$> parse (decodeHex "D8005AC2A8F0")
+Just 1
+
+>>> eval <$> parse (decodeHex "F600BC2D8F")
+Just 0
+
+>>> eval <$> parse (decodeHex "9C005AC2F8F0")
+Just 0
+
+>>> eval <$> parse (decodeHex "9C0141080250320F1802104A08")
+Just 1
+
 -}
 module Main (main) where
 
 import Advent (fromDigits, format)
-import Control.Applicative (many)
 import Data.Char (digitToInt)
 import Text.ParserCombinators.ReadP (ReadP)
 import Text.ParserCombinators.ReadP qualified as ReadP
@@ -25,66 +69,95 @@ import Text.ParserCombinators.ReadP qualified as ReadP
 main :: IO ()
 main =
  do inp <- [format|16 %s%n|]
-    let p = parse (decodeHex =<< inp)
+    let Just p = parse (decodeHex inp)
     print (vers p)
     print (eval p)
 
-data Packet = Lit Int Int | Op Int Int [Packet]
+-- | A /BITS/ packet
+data Packet
+  = Lit Int Int -- ^ Literal with version and value
+  | Op Int Int [Packet] -- ^ Operator with version, opcode, and arguments
   deriving Show
 
--- | Compute the sum of the versions
+-- | Compute the sum of the versions of all nested packets
 vers :: Packet -> Int
 vers (Lit v _   ) = v
 vers (Op  v _ xs) = v + sum (map vers xs)
 
--- | Evaluate the expression
+-- | Evaluate the packet as an expression
 eval :: Packet -> Int
-eval (Lit _ n) = n
-eval (Op _ 0 xs) = sum     (eval <$> xs)
-eval (Op _ 1 xs) = product (eval <$> xs)
-eval (Op _ 2 xs) = minimum (eval <$> xs)
-eval (Op _ 3 xs) = maximum (eval <$> xs)
-eval (Op _ 5 [x,y]) = if eval x >  eval y then 1 else 0
-eval (Op _ 6 [x,y]) = if eval x <  eval y then 1 else 0
-eval (Op _ 7 [x,y]) = if eval x == eval y then 1 else 0
+eval (Lit _ n     ) = n
+eval (Op _ 0 xs   ) = sum      (eval <$> xs     )
+eval (Op _ 1 xs   ) = product  (eval <$> xs     )
+eval (Op _ 2 xs   ) = minimum  (eval <$> xs     )
+eval (Op _ 3 xs   ) = maximum  (eval <$> xs     )
+eval (Op _ 5 [x,y]) = fromEnum (eval x >  eval y)
+eval (Op _ 6 [x,y]) = fromEnum (eval x <  eval y)
+eval (Op _ 7 [x,y]) = fromEnum (eval x == eval y)
 eval o = error ("bad expression: " ++ show o)
 
-parse :: String -> Packet
-parse xs =
-  case ReadP.readP_to_S pPacket xs of
-    [(p,_)] -> p
-    _ -> error ("Failed parsing: " ++ show xs)
+-- | Parser for strings of @'1'@ and @'0'@ to 'Packet'
+--
+-- >>> parse "110100101111111000101000"
+-- Just (Lit 6 2021)
+--
+-- >>> parse "00111000000000000110111101000101001010010001001000000000"
+-- Just (Op 1 6 [Lit 6 10,Lit 2 20])
+--
+-- >>> parse "11101110000000001101010000001100100000100011000001100000"
+-- Just (Op 7 3 [Lit 2 1,Lit 4 2,Lit 1 3])
+parse :: String -> Maybe Packet
+parse (ReadP.readP_to_S pPacket -> [(p,_)]) = Just p
+parse _ = Nothing
+
+-- | Decode a hex string into bit string
+--
+-- >>> decodeHex "D2FE28"
+-- "110100101111111000101000"
+--
+-- >>> decodeHex "38006F45291200"
+-- "00111000000000000110111101000101001010010001001000000000"
+decodeHex :: String -> String
+decodeHex = concatMap \case
+  '0' -> "0000"; '1' -> "0001"; '2' -> "0010"; '3' -> "0011"
+  '4' -> "0100"; '5' -> "0101"; '6' -> "0110"; '7' -> "0111"
+  '8' -> "1000"; '9' -> "1001"; 'A' -> "1010"; 'B' -> "1011"
+  'C' -> "1100"; 'D' -> "1101"; 'E' -> "1110"; 'F' -> "1111"
+  x -> error ("decodeHex: bad argument " ++ show x)
+
+-- * 'ReadP' parser combinators
 
 -- | Parse a single packet
 pPacket :: ReadP Packet
 pPacket =
- do v <- field 3
-    t <- field 3
+ do v <- field 3; t <- field 3
     if t == 4
       then Lit v   <$> pLiteral
       else Op  v t <$> pArguments
 
--- | Parse a fixed-width big-endian, binary number
-field :: Int -> ReadP Int
+-- | Parse an @n@-bit fixed-width big-endian, binary number
+field :: Int {- ^ bit width -} -> ReadP Int
 field n = fromDigits 2 . map digitToInt <$> ReadP.count n ReadP.get
 
--- | Parse a variable-sized number chunk
+-- | Parse a single bit as a boolean flag
+flag :: ReadP Bool
+flag = ('1' ==) <$> ReadP.get
+
+-- | Parse a variable-sized number in 4-bit chunks
 pLiteral :: ReadP Int
 pLiteral = go 0
   where
     go acc =
-     do more <- (1==) <$> field 1
-        chunk <- field 4
+     do more <- flag; chunk <- field 4
         (if more then go else pure) (16 * acc + chunk)
 
--- | Parse a list of sub-packets
+-- | Parse a list of sub-packets either by packet count or bit-size
 pArguments :: ReadP [Packet]
 pArguments =
- do byCount <- (1==) <$> field 1
-    if byCount
-      then do n <- field 11
-              ReadP.count n pPacket
-      else pSized =<< field 15
+ do mode <- flag
+    if mode
+      then do n <- field 11; ReadP.count n pPacket
+      else do n <- field 15; pSized n
 
 -- | Parse a list of packets that fit exactly in @n@ bits
 pSized :: Int -> ReadP [Packet]
@@ -94,23 +167,3 @@ pSized n =
     GT -> do (str, p) <- ReadP.gather pPacket
              (p:) <$> pSized (n - length str)
     EQ -> pure []
-
--- | Decode a hex character into 4 bits
-decodeHex :: Char -> String
-decodeHex '0' = "0000"
-decodeHex '1' = "0001"
-decodeHex '2' = "0010"
-decodeHex '3' = "0011"
-decodeHex '4' = "0100"
-decodeHex '5' = "0101"
-decodeHex '6' = "0110"
-decodeHex '7' = "0111"
-decodeHex '8' = "1000"
-decodeHex '9' = "1001"
-decodeHex 'A' = "1010"
-decodeHex 'B' = "1011"
-decodeHex 'C' = "1100"
-decodeHex 'D' = "1101"
-decodeHex 'E' = "1110"
-decodeHex 'F' = "1111"
-decodeHex x = error ("bad hex: " ++ show x)
