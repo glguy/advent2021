5ef647cd6923518a5c6ae39b14176b4c93c6a350
Eric Mertens <emertens@galois.com>
2021-12-20 00:08:11 -0600

    20

--- execs/Day20.hs
+++ execs/Day20.hs
@@ -1,4 +1,4 @@
-{-# Language LambdaCase, BlockArguments, ImportQualifiedPost, QuasiQuotes #-}
+{-# Language LambdaCase, BlockArguments, ImportQualifiedPost, NumericUnderscores, BinaryLiterals, QuasiQuotes #-}
 {-|
 Module      : Main
 Description : Day 20 solution
@@ -11,41 +11,44 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent
-import Data.Map qualified as Map
+import Advent (ordNub, times, fromDigits, format)
+import Advent.Coord(Coord(..), coordLines)
+import Data.IntSet (IntSet)
+import Data.IntSet qualified as IntSet
 import Data.Set (Set)
 import Data.Set qualified as Set
 
-import Advent.Coord
+-- | Pictures have the boolean value at all of the coordinates in
+-- the set, and have the complement value at any other coordinate.
+data Picture = Picture !Bool !(Set Coord)
 
 main :: IO ()
 main =
-  do (alg, img) <- [format|20 %s%n%n(%s%n)*|]
-     let alg1 = Set.fromList [i | (i,'#') <- zip [0..] alg]
-     let img1 = Map.keysSet (Map.filter ('#'==) (Map.fromList $ coordLines img))
+  do (algStr, imgStrs) <- [format|20 %s%n%n(%s%n)*|]
+     let alg = IntSet.fromList [i | (i,'#') <- zip [0..] algStr]
+     let img = Picture True (Set.fromList [c | (c, '#') <- coordLines imgStrs])
 
-     let Picture True out1 = times 2 (step alg1) (Picture True img1)
-     print $ length out1
-     let Picture True out2 = times 50 (step alg1) (Picture True img1)
-     print $ length out2
+     let Picture True out1 = times 2 (step alg) img
+     print (length out1)
 
+     let Picture True out2 = times 50 (step alg) img
+     print (length out2)
 
-step :: Set Int -> Picture -> Picture
-step alg (Picture def img) = Picture def' img'
+-- | Apply the given image enhancement algorithm to a picture
+step :: IntSet -> Picture -> Picture
+step alg (Picture val img) = Picture val' img'
   where
+    val'
+      | val       = IntSet.member 0b111_111_111 alg
+      | otherwise = IntSet.member 0b000_000_000 alg
+
     img' = Set.fromList
       [  x
-        | x <- ordNub (concatMap cell (Set.toList img))
-        , let n = fromDigits 2
-                   [if (if Set.member i img then def else not def) then 1 else 0  | i <- cell x]
-                   , Set.member n alg == def'
+        | x <- ordNub (concatMap cell img)
+        , let n = fromDigits 2 [if Set.member i img == val then 1 else 0  | i <- cell x]
+        , IntSet.member n alg == val'
       ]
-    def'
-      | def = Set.member 511 alg
-      | otherwise = Set.member 0 alg
 
+-- | 3x3 neighborhood around a coordinate
 cell :: Coord -> [Coord]
 cell (C y x) = C <$> [y-1 .. y+1] <*> [x-1 .. x+1]
-
-
-data Picture = Picture !Bool !(Set Coord)
