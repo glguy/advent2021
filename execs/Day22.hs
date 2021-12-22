84aa1f3eb4400ac245595f121ed1efd7c8f22264
Eric Mertens <emertens@galois.com>
2021-12-22 14:56:40 -0600

    comments

--- execs/Day22.hs
+++ execs/Day22.hs
@@ -35,7 +35,7 @@ main =
  do inp <- [format|22 (@C x=%d..%d,y=%d..%d,z=%d..%d%n)*|]
     let seg lo hi = Seg lo (hi+1) -- make upper limit exclusive
         steps = [ (c, seg x1 x2 :* seg y1 y2 :* seg z1 z2 :* Pt)
-                | (c,x1,x2,y1,y2,z1,z2) <- inp]
+                | (c, x1, x2, y1, y2, z1, z2) <- inp]
         p1seg = seg (-50) 50
         p1cube = p1seg :* p1seg :* p1seg :* Pt
     print (solve (mapMaybe (traverse (intersectBox p1cube)) steps))
@@ -70,6 +70,10 @@ intersectSeg (Seg alo ahi) (Seg blo bhi)
     lo = max alo blo
     hi = min ahi bhi
 
+-- | Determine if a value falls within a segment.
+inSeg :: Int -> Seg -> Bool
+inSeg x (Seg lo hi) = lo <= x && x < hi
+
 -- * N-dimensional boxes
 
 -- | Natural numbers (used for type index)
@@ -82,30 +86,44 @@ data Box :: N -> Type where
 
 infixr 6 :* -- a little higher than list cons
 
+instance Show (Box n) where
+  showsPrec _ Pt = showString "Pt"
+  showsPrec p (x :* xs) = showParen (p > 6) (shows x . showString " :* " . showsPrec 6 xs)
+
 -- | Returns the number of points contained in a box.
+--
+-- >>> size (Seg 1 4 :* Seg 0 3 :* Seg 0 2 :* Pt)
+-- 18
 size :: Box n -> Int
 size Pt         = 1
 size (s :* box) = len s * size box
 
--- | Find the intersection between two boxes.
+-- | The intersection of two boxes is the intersection of their segments.
 intersectBox :: Box n -> Box n -> Maybe (Box n)
-intersectBox Pt        Pt        = pure Pt
-intersectBox (x :* xs) (y :* ys) = (:*) <$> intersectSeg x y <*> intersectBox xs ys
+intersectBox = traverseBox2 intersectSeg
 
 -- | Subtract the second box from the first box returning a list of boxes
 -- that cover all the remaining area.
+--
+-- >>> subBox (Seg 2 3 :* Pt) (Seg 0 4 :* Pt)
+-- [Seg 0 2 :* Pt,Seg 3 4 :* Pt]
+--
+-- >>> subBox (Seg 3 5 :* Pt) (Seg 0 4 :* Pt)
+-- [Seg 0 3 :* Pt]
 subBox ::
   Box n {- ^ remove this -} ->
   Box n {- ^ from this -} ->
   [Box n]
 subBox b1 b2
   | isNothing (intersectBox b1 b2) = [b2]
-  | otherwise = [b | (b, All False) <- runWriterT (go b1 b2)]
+  | otherwise = [b | (b, All False) <- runWriterT (traverseBox2 segs b1 b2)]
   where
-    segs (Seg a b) (Seg c d) =
-      let xs = [c] ++ [a | c < a, a < d] ++ [b | c < b, b < d] ++ [d]
-      in writerT [(Seg lo hi, All (a <= lo && lo < b)) | lo <- xs | hi <- tail xs]
+    segs s1@(Seg a b) s2@(Seg c d) =
+      writerT [(Seg lo hi, All (lo `inSeg` s1)) | lo <- xs | hi <- tail xs]
+      where
+        xs = [c] ++ [a | a `inSeg` s2] ++ [b | b `inSeg` s2] ++ [d]
 
-    go :: Box n -> Box n -> WriterT All [] (Box n)
-    go Pt        Pt        = pure Pt
-    go (x :* xs) (y :* ys) = (:*) <$> segs x y <*> go xs ys
+-- | Zip two boxes together.
+traverseBox2 :: Applicative f => (Seg -> Seg -> f Seg) -> Box n -> Box n -> f (Box n)
+traverseBox2 f (x :* xs) (y :* ys) = (:*) <$> f x y <*> traverseBox2 f xs ys
+traverseBox2 _ Pt        Pt        = pure Pt
