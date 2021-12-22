7ca28755f5a0107a8a01892827507d6213a3922a
Eric Mertens <emertens@galois.com>
2021-12-22 12:03:03 -0600

    22

--- execs/Day22.hs
+++ execs/Day22.hs
@@ -18,8 +18,10 @@ module Main (main) where
 import Advent.Format (format)
 import Control.Monad.Trans.Writer.CPS (runWriterT, writerT, WriterT)
 import Data.Kind (Type)
-import Data.Maybe (isNothing)
+import Data.List (tails)
+import Data.Maybe (isNothing, mapMaybe)
 import Data.Monoid (All(All))
+import Control.Monad
 
 -- | On and off commands from the input file
 data C = Con | Coff
@@ -38,16 +40,15 @@ main =
                 | (c,x1,x2,y1,y2,z1,z2) <- inp]
         p1seg = seg (-50) 50
         p1cube = p1seg :× p1seg :× p1seg :× Pt
-    print $ solve [(cmd, cube') | (cmd, cube) <- steps, Just cube' <- [intersectBox p1cube cube]]
+    print (solve (mapMaybe (traverse (intersectBox p1cube)) steps))
     print (solve steps)
 
 -- | Figure out how many lights the given instructions turn on.
+--
+-- Each @on@ command has all future boxes subtracted from it before
+-- being included in the sum.
 solve :: [(C, Box n)] -> Int
-solve [] = 0
-solve ((Coff,_):xs) = solve xs
-solve ((Con,c):xs) = sum (map size c') + solve xs
-  where
-    c' = foldl (\acc y -> subbox y =<< acc) [c] (map snd xs)
+solve xs = sum [size c | (Con,y):ys <- tails xs, c <- foldM subbox y (map snd ys)]
 
 -- * Segments
 
@@ -67,33 +68,41 @@ intersectSeg (Seg alo ahi) (Seg blo bhi)
     lo = max alo blo
     hi = min ahi bhi
 
--- * Generalized N-dimensional Boxes
+-- * N-dimensional boxes
 
 -- | Natural numbers (used for type index)
 data N = S N | Z
 
+-- | An n-dimensional box.
 data Box :: N -> Type where
-  Pt   :: Box 'Z
-  (:×) :: Seg -> Box n -> Box ('S n)
+  Pt   :: Box 'Z -- ^ A single point
+  (:×) :: Seg -> Box n -> Box ('S n) -- ^ A box extended along an axis
 
-infixr 4 :×
+infixr 6 :× -- a little higher than list cons
 
+-- | Returns the number of points contained in a box.
 size :: Box n -> Int
 size Pt         = 1
 size (s :× box) = len s * size box
 
+-- | Find the intersection between two boxes.
 intersectBox :: Box n -> Box n -> Maybe (Box n)
 intersectBox Pt        Pt        = pure Pt
 intersectBox (x :× xs) (y :× ys) = (:×) <$> intersectSeg x y <*> intersectBox xs ys
 
-subbox :: Box n -> Box n -> [Box n]
+-- | Subtract the second box from the first box returning a list of boxes
+-- that cover all the remaining area.
+subbox ::
+  Box n ->
+  Box n {- ^ remove -} ->
+  [Box n]
 subbox b1 b2
-  | isNothing (intersectBox b1 b2) = [b2]
+  | isNothing (intersectBox b1 b2) = [b1]
   | otherwise = [b | (b, All False) <- runWriterT (go b1 b2)]
   where
     segs (Seg a b) (Seg c d) =
-      let xs = [c] ++ [a | c < a, a < d] ++ [b | c < b, b < d] ++ [d]
-      in writerT [(Seg lo hi, All (a <= lo && lo < b)) | lo <- xs | hi <- tail xs]
+      let xs = [a] ++ [c | a < c, c < b] ++ [d | a < d, d < b] ++ [b]
+      in writerT [(Seg lo hi, All (c <= lo && lo < d)) | lo <- xs | hi <- tail xs]
 
     go :: Box n -> Box n -> WriterT All [] (Box n)
     go Pt        Pt        = pure Pt
