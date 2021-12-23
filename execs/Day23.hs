36c7ddc071b08f2784eb64ea90b0bacb85c14ccb
Eric Mertens <emertens@galois.com>
2021-12-23 15:31:03 -0600

    23

--- execs/Day23.hs
+++ execs/Day23.hs
@@ -14,74 +14,68 @@ to get all the lizards back into the correct rooms.
 -}
 module Main (main) where
 
-import Advent.Coord
-    ( Coord(..), coordCol, below, manhattan, cardinal )
-import Advent.Input ( getInputMap )
-import Advent.Search (AStep(AStep), dfs, astar)
-import Data.List (find)
+import Advent.Coord (Coord(..), coordCol, below, manhattan, cardinal)
+import Advent.Input (getInputMap)
+import Advent.Search (AStep(..), dfs, astar)
 import Data.Map (Map)
 import Data.Map qualified as Map
+import Data.Char (ord, isLetter)
 
-main :: IO ()
-main =
- do inp <- Map.filter (`elem` ".ABCD") <$> getInputMap 23
-    print (head [cost | (w, cost) <- astar step inp, done w])
+data Cell = Open | Amphi { amphiTarget :: !Int, amphiCost :: !Int }
+  deriving (Eq, Ord, Show)
 
+-- | Compute all the information needed from a character in the input map.
+toCell :: Char -> Maybe Cell
+toCell '.' = Just Open
+toCell a | isLetter a = Just $! Amphi (3 + 2 * (ord a - 65)) (10 ^ (ord a - 65))
+toCell _ = Nothing
+
+-- | Predicate for rooms (rather than hallways)
 isRoom :: Coord -> Bool
 isRoom (C _ c) = c == 3 || c == 5 || c == 7 || c == 9
 
-step :: Map Coord Char -> [AStep (Map Coord Char)]
+main :: IO ()
+main =
+ do inp <- Map.mapMaybe toCell <$> getInputMap 23
+    print (head [cost | (w, cost) <- astar step inp, done w])
+
+-- | Step the simulation once tracking the cost of the move.
+step :: Map Coord Cell -> [AStep (Map Coord Cell)]
 step w =
-  [ AStep w' (manhattan c dest * tokCost tok) 0
-  | (c, tok) <- Map.toList w
-  , tok `elem` "ABCD"
+  [ AStep { astepNext = Map.insert c Open (Map.insert dest a w)
+          , astepCost = manhattan c dest * stepCost
+          , astepHeuristic = 0 }
+  | (c, a@(Amphi target stepCost)) <- Map.toList w
   , dest <- route w c
-    
   , if isRoom c
       then not (isRoom dest)
-        && not (roomClean w (coordCol c) (target (coordCol c)))
+        && not (roomClean w (coordCol c))
       else isRoom dest
-        && coordCol dest == roomCol tok
-        && roomClean w (roomCol tok) tok
-        && Map.findWithDefault tok (below dest) w == tok
-
-  , let w' = Map.insert c '.'
-           $ Map.insert dest tok w
+        && coordCol dest == target
+        && roomClean w target
+        && maybe True (a==) (Map.lookup (below dest) w)
   ]
 
-target :: Int -> Char
-target 3 = 'A'
-target 5 = 'B'
-target 7 = 'C'
-target 9 = 'D'
-
-roomClean :: Map Coord Char -> Int -> Char -> Bool
-roomClean w c tok = roomCheck w c (`elem` [tok,'.'])
-
-done :: Map Coord Char -> Bool
-done w = all (\tok -> roomCheck w (roomCol tok) (tok ==)) "ABCD"
-
-roomCheck :: Map Coord Char -> Int -> (Char -> Bool) -> Bool
-roomCheck w c p = go 2
+-- | Check that all the amphis in a room are supposed to be there.
+roomClean :: Map Coord Cell -> Int -> Bool
+roomClean w c = go 2
   where
     go r =
       case Map.lookup (C r c) w of
+        Just Open -> go (r+1)
+        Just (Amphi t _) -> t == c && go (r+1)
         Nothing -> True
-        Just x -> p x && go (r+1)
 
-roomCol :: Char -> Int
-roomCol 'A' = 3
-roomCol 'B' = 5
-roomCol 'C' = 7
-roomCol 'D' = 9
+-- | Check that everything on the map is where it should be.
+done :: Map Coord Cell -> Bool
+done w =
+  all (\(k,v) ->
+      case v of
+        Open -> True
+        Amphi t _ -> coordCol k == t)
+  (Map.toList w)
 
-tokCost :: Char -> Int
-tokCost 'A' = 1
-tokCost 'B' = 10
-tokCost 'C' = 100
-tokCost 'D' = 1000
-
-route :: Map Coord Char -> Coord -> [Coord]
+route :: Map Coord Cell -> Coord -> [Coord]
 route w = dfs move
   where
-    move c = [c' | c' <- cardinal c, Map.lookup c' w == Just '.']
+    move c = [c' | c' <- cardinal c, Map.lookup c' w == Just Open]
