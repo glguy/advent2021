1367c646b0449f2117781b51e77347a9d3824e37
Eric Mertens <emertens@galois.com>
2021-12-24 08:58:03 -0600

    24

--- execs/Day24.hs
+++ execs/Day24.hs
@@ -1,4 +1,4 @@
-{-# Language BangPatterns #-}
+{-# Language BangPatterns, ViewPatterns #-}
 {-|
 Module      : Main
 Description : Day 24 solution
@@ -11,14 +11,15 @@ Maintainer  : emertens@gmail.com
 -}
 module Main (main) where
 
-import Advent (getInputLines, chunks)
+import Advent (getInputLines, chunks, fromDigits)
 import Data.Char (intToDigit)
+import Text.Read (readMaybe)
 
 main :: IO ()
 main =
  do inp <- map extract . chunks 18 . map words <$> getInputLines 24
-    putStrLn $ map intToDigit $ head $ solve [9,8..1] 0 inp
-    putStrLn $ map intToDigit $ head $ solve [1,2..9] 0 inp
+    print $ fromDigits 10 $ head $ solve [9,8..1] 0 inp
+    print $ fromDigits 10 $ head $ solve [1,2..9] 0 inp
 
 solve :: [Int] -> Int -> [(Int, Int, Int)] -> [[Int]]
 solve guesses !z ((a,b,c):bs) =
@@ -30,25 +31,25 @@ solve _ z [] = [[] | z == 0]
      
 extract :: [[String]] -> (Int, Int, Int)
 extract [
-    ["inp","w"],
-    ["mul","x","0"],
-    ["add","x","z"],
-    ["mod","x","26"],
-    ["div","z",a],
-    ["add","x",b],
-    ["eql","x","w"],
-    ["eql","x","0"],
-    ["mul","y","0"],
-    ["add","y","25"],
-    ["mul","y","x"],
-    ["add","y","1"],
-    ["mul","z","y"],
-    ["mul","y","0"],
-    ["add","y","w"],
-    ["add","y",c],
-    ["mul","y","x"],
-    ["add","z","y"]] =
-    (read a, read b, read c)
+  ["inp", "w"      ],
+  ["mul", "x", "0" ],
+  ["add", "x", "z" ],
+  ["mod", "x", "26"],
+  ["div", "z", readMaybe -> Just a],
+  ["add", "x", readMaybe -> Just b],
+  ["eql", "x", "w" ],
+  ["eql", "x", "0" ],
+  ["mul", "y", "0" ],
+  ["add", "y", "25"],
+  ["mul", "y", "x" ],
+  ["add", "y", "1" ],
+  ["mul", "z", "y" ],
+  ["mul", "y", "0" ],
+  ["add", "y", "w" ],
+  ["add", "y", readMaybe -> Just c],
+  ["mul", "y", "x" ],
+  ["add", "z", "y" ]] =
+  (a, b, c)
 extract x = error (show x)
 
 impl :: Int -> Int -> Int -> Int -> Int -> Int
